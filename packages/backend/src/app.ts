b83cb7ec05dcf8c302973e99c2eb6d0570eabca6
sbhbenjamin <sbhbenjamin@gmail.com>
2023-10-06 21:01:06 +0800

    add eslint

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,12 +1,10 @@
-import express, { Request, Response } from 'express';
+import type { Request, Response } from "express";
+import express from "express";
 
 const app = express();
-const PORT = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, World from TypeScript!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World from TypeScript!");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+export default app;
